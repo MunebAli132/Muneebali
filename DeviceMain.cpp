224c7f622312b22a9bbf06e2aeaa43bfa6409460
Muneeb-Ali-Paki <63760372+Muneeb-Ali-Paki@users.noreply.github.com>
2020-04-17 01:53:23 +0200

    Update DeviceMain.cpp

--- DeviceMain.cpp
+++ DeviceMain.cpp
@@ -6,9 +6,9 @@
 int main()
 {
 	const unsigned maxEquipment = 4;
-	Device* equipments[maxEquipment];
+	Device* Device[maxEquipment];
 
-	equipments[0] = new Equipment("932-11", 200);	 // This instruction should result in compiler error. Why?
+	equipments[0] = new Device("932-11", 200);	 // This instruction should result in compiler error. Why?
 	equipments[1] = new Printer("182-19", 120., 8);	 // Parameters: unique ID, price, printing speed (pages per minute)
 	equipments[2] = new Display("18A", 200.1, 2003); // Parameters: unique ID, price, year of appropriation
 	equipments[3] = new Printer("14-932-22", 31.5, 15.4);   // Parameters: unique ID, price, printing speed
